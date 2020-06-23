6e9663e38d030faf4f12b11efb79c698584af0ee
D3ad5h0t <alexgl007@mail.ru>
2020-06-23 16:33:06 +1000

    added SingletonTester

---
+++ SingletonTester/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SingletonTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class SingletonTester
+    {
+        public static bool IsSingleton(Func<object> func)
+        {
+            var first = func();
+            var second = func();
+
+            return ReferenceEquals(first, second);
+        }
+    }
+}
